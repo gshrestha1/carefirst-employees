e8174fdc9a1ee9f09eedee0e25a87346f49370a6
Gopal Shrestha <gopal_shrestha@optum.com>
2022-07-05 22:37:31 -0400

    Added Swagger2 documentation

--- src/main/java/com/carefirst/employees/EmployeeApplication.java
+++ src/main/java/com/carefirst/employees/EmployeeApplication.java
@@ -2,11 +2,13 @@ package com.carefirst.employees;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableSwagger2
 public class EmployeeApplication {
 
     @PostConstruct
